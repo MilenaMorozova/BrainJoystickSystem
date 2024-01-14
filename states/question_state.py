e9cafbe96e04d9dd55af597ed4e4ba5a030cb18e
ISMashtakov <ismashtakov@edu.hse.ru>
2024-01-14 21:52:20 +0300

    Добавил показ ответа, если никто не ответил правильно (#19)

--- states/question_state.py
+++ states/question_state.py
@@ -1,5 +1,7 @@
+from animations.show_answer_animation import ShowAnswerAnimation
 from enums.joystick_button_enum import JoystickButton
 from enums.status_enum import StatusEnum
+from helpers.timer import OnEndSignalArgs
 from services.joystick_input import OnPlayerClickSignalArgs
 from states.state_with_service_locator import StateWithServiceLocator
 
@@ -10,10 +12,18 @@ class QuestionState(StateWithServiceLocator):
     def on_enter(self):
         self.locator.question_timer.start()
         self.locator.input.on_player_click.connect(self._on_player_click_handler)
+        self.locator.question_timer.on_end.connect(self._on_timer_end)
 
     def on_exit(self):
         self.locator.question_timer.pause()
         self.locator.input.on_player_click.disconnect(self._on_player_click_handler)
+        self.locator.question_timer.on_end.disconnect(self._on_timer_end)
+
+    def _on_timer_end(self, args: OnEndSignalArgs):
+        question = self.locator.game.selected_question
+        question.is_answered = True
+        self.show_answer_animation = ShowAnswerAnimation(question)
+        self._set_next_state_after_animation(StatusEnum.CHOICE_QUESTION, self.show_answer_animation)
 
     def _on_player_click_handler(self, args: OnPlayerClickSignalArgs):
         if (
